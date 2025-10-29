7a5823305e746f43507a4c141f6d37a70998c43b
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-10-29 13:42:29 +0545

    wrote the validation for news

---
+++ src/validations/newsValidation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+export const newsSchema = z.object({
+    id: z.number().int(),
+    userId: z.number().int(),
+    title: z.string(),
+    content: z.string(),
+    coverImg: z.string().optional(),
+    createdAt: z.date(),
+    updatedAt: z.date()
+})
+
+export const createNewsSchema = z.object({
+    title: z.string(),
+    content: z.string(),
+    coverImg: z.string().optional()
+})
+
+export const updateNewsSchema = z.object({
+    title: z.string().nullable(),
+    content: z.string().nullable(),
+    coverImg: z.string().nullable()
+})
+
