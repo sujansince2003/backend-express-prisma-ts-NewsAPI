12a13f7f9eb2ac047869b82a1c3ed40284d3710a
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-10-30 08:07:47 +0545

    Added newsRouter and static file support for cover images in uploads directory.

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import fileupload from "express-fileupload"
 import userRouter from "./routes/user.route";
 import cookieParser from 'cookie-parser';
 import profileRouter from "./routes/profile.route";
-
+import newsRouter from "./routes/news.route"
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/uploads", express.static("src/uploads/images"));
+app.use("/uploads", express.static("src/uploads/images"));     //for profile pictures
+app.use("/uploads", express.static("src/uploads/coverimgs"));   //for coverimgs
 
 // http://localhost:8000/uploads/{filename}
 
@@ -18,6 +19,7 @@ app.use(fileupload());
 app.use(cors());
 app.use("/api/user", userRouter);
 app.use("/api/profile", profileRouter);
+app.use("/api/news", newsRouter);
 
 
 app.get("/", (req: Request, res: Response) => {
