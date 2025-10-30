a18b61bac96f2413750a1eef28ee0f8b0abad4cb
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-10-30 12:04:19 +0545

    defined route for getting all the news

--- src/routes/news.route.ts
+++ src/routes/news.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { CreateNews } from "../controllers/news.controller";
+import { CreateNews, getNews } from "../controllers/news.controller";
 import verifyToken from "../middlwares/auth.middleware";
 
 const router = Router()
 
+
+router.get("/allnews", getNews)
 router.post("/createnews", verifyToken, CreateNews)
 
 
