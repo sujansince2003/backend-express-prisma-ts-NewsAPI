390b855f62385ea8d99f3d013fbf9dc51c27dbd9
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-10-30 08:00:33 +0545

    controller function to create news

---
+++ src/controllers/news.controller.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+import { createNewsSchema } from "../validations/newsValidation";
+import prisma from "../db/db.config";
+import { UploadedFile } from "express-fileupload";
+import { ImgValidator, uniqueIdGenerator } from "../utils/helper.utils";
+
+type newsDataType = {
+    title: string,
+    content: string
+
+}
+
+export async function CreateNews(req: Request, res: Response) {
+
+
+    try {
+
+
+        const userId = req.userId;
+        if (!userId) {
+            res.status(400).json({
+                message: "User ID is required",
+                data: []
+            })
+            return
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(userId)
+            }
+        })
+        if (!user) {
+            res.status(400).json({
+                message: "User not found",
+                data: []
+            })
+            return
+        }
+
+
+        const newsData = req.body;
+        const validateNewsData = createNewsSchema.safeParse(newsData);
+        if (!validateNewsData.success) {
+            res.status(400).json({
+                message: "Validation failed for news data",
+                errors: validateNewsData.error.message
+            })
+            return
+        }
+        const { title, content } = validateNewsData.data;
+
+        const coverImg = req.files?.coverImg as UploadedFile;
+        const isvalidImg = ImgValidator(coverImg);
+        if (!isvalidImg.valid) {
+            res.status(400).json({
+                message: isvalidImg.error
+            })
+            return;
+        }
+
+        const imgExtension = coverImg?.name.split(".");
+        const uniquecoverimgname = uniqueIdGenerator() + "." + imgExtension[1];
+        const uploadPath = process.cwd() + "/src/uploads/coverimgs/" + uniquecoverimgname
+        coverImg.mv(uploadPath, (err) => {
+            if (err) throw err
+        })
+
+
+
+
+
+
+
+        const createNewsData = await prisma.news.create({
+            data: {
+                title: title,
+                content: content,
+                coverImg: uniquecoverimgname,
+                userId: Number(userId)
+            }
+        })
+
+        res.status(201).json({
+            message: "news created successfully!!",
+            data: createNewsData
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error occured",
+            errorMsg: error
+        })
+    }
+}
