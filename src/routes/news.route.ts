ecbe626c7ffa4af84e958550c59aa10a5cbb2036
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-11-09 18:13:33 +0545

    add updateNews route to news API with token verification

--- src/routes/news.route.ts
+++ src/routes/news.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { CreateNews, getNews, getAllNews } from "../controllers/news.controller";
+import { CreateNews, getNews, getAllNews, updateNews } from "../controllers/news.controller";
 import verifyToken from "../middlwares/auth.middleware";
 
 const router = Router()
@@ -8,6 +8,7 @@ const router = Router()
 router.get("/allnews", getAllNews)
 router.get("/getnews/:newsid", getNews)
 router.post("/createnews", verifyToken, CreateNews)
+router.post("/updatenews", verifyToken, updateNews)
 
 
 export default router
