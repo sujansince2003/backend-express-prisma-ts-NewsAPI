565906aee6ecfde2db11fcabca85facaf275643c
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-11-09 19:17:36 +0545

    add express-rate-limit and helmet for enhanced security and rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
+import helmet from "helmet";
 import fileupload from "express-fileupload"
 import userRouter from "./routes/user.route";
 import cookieParser from 'cookie-parser';
 import profileRouter from "./routes/profile.route";
 import newsRouter from "./routes/news.route"
+import { rateLimiter } from "./utils/ratelimit";
+
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(fileupload());
+app.use(cors());
+app.use(rateLimiter)
 app.use("/uploads/images", express.static("src/uploads/images"));     //for profile pictures
 app.use("/uploads/coverimgs", express.static("src/uploads/coverimgs"));   //for coverimgs
 
 // http://localhost:8000/uploads/coverimgs/{filename}
 
-app.use(fileupload());
-app.use(cors());
+
 app.use("/api/user", userRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/news", newsRouter);
