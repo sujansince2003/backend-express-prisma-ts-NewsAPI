9badbf78bf58d53cca9610ae4187a1803e1264f5
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-11-11 10:35:36 +0545

    add Redis client and axios for fetching todos; update rate limiter to allow 10 requests per minute

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { RegisterUser, LoginUser, GetAllUsers, LogoutUser } from "../controllers/auth.controller";
 import verifyToken from "../middlwares/auth.middleware";
+import Redisclient from "../redisClient/client.redis";
+import axios from "axios";
 
 
 const userRouter = Router();
@@ -17,5 +19,33 @@ userRouter.get("/profile", verifyToken, (req, res) => {
         }
     });
 });
+
+userRouter.get("/todos", async (req, res) => {
+    const todosfromredis = await Redisclient.get("todos")
+    if (todosfromredis) {
+        const tododata = JSON.parse(todosfromredis)
+
+
+        res.json({
+            msg: "got from redis",
+            data: tododata
+
+        })
+        return
+    }
+    const { data: todos } = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    const todosdata = todos
+    await Redisclient.set("todos", JSON.stringify(todosdata), "EX", 60)
+    res.json({
+        msg: "got from api",
+        data: todosdata
+    })
+    return
+
+
+
+})
+
+
 
 export default userRouter;
